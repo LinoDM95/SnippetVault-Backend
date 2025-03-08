d7c5a0b10584f7b2b41d87d38d09bcfd13e1b9a3
LinoDM95 <linodemarco95@gmail.com>
2025-03-08 23:14:50 +0100

    Sync

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -10,11 +10,11 @@ CSRF_TRUSTED_ORIGINS = ["https://"+os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-MIDDLEWARE = [
+MMIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
